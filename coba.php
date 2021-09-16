95d65d0c9836bc666218e4abe91ceb7c03952436
MRizkyF27 <63577711+Rizky2705@users.noreply.github.com>
2021-09-16 09:49:42 +0700

    video 1
    menyelesaikan latihan dari video pertama

--- coba.php
+++ coba.php
@@ -9,4 +9,6 @@ $a = new Coba();
 $b = new Coba();
 $c = new Coba();
 
-var_dump($a);
+var_dump($a);var_dump($a);
+var_dump($b);
+var_dump($c);
---
+++ produk.php
@@ -0,0 +1,11 @@
+<?php
+
+// Jualan Pproduk
+// Komik
+// Game
+class Produk{
+
+}
+
+$produk1 = new Produk();
+$produk2 = new Produk();
