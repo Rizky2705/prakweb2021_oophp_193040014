29286686d4c1f8d6e9600d01af91ff1c73f6e9bc
MRizkyF27 <63577711+Rizky2705@users.noreply.github.com>
2021-09-16 10:42:29 +0700

    Video 4
    belajar menggunanakn Constructor

---
+++ constructor.php
@@ -0,0 +1,33 @@
+<?php
+
+// Jualan Pproduk
+// Komik
+// Game
+class Produk{
+    public $judul,
+           $penulis,
+           $penerbit,
+           $harga;
+
+    public function __construct($judul = "Judul", $penulis = "Pennulis", $penerbit = "Penerbit", $harga = 0){
+       $this->judul = "$judul";
+       $this->penulis = "$penulis";
+       $this->penerbit = "$penerbit";
+       $this->harga = "$harga";
+    }
+
+    public function getLabel(){
+        return "$this->penulis, $this->penerbit";
+        }
+
+}
+
+$produk1 = new Produk("Naruto", "Masasshi Kishimoto", "Shonen Jump", 30000);
+$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000);
+$produk3 = new Produk("Dragon Ball");
+
+echo "komik : " . $produk1->getLabel();
+echo "<br>";
+echo "Game : " . $produk2->getLabel();
+echo "<br>";
+var_dump($produk3);
